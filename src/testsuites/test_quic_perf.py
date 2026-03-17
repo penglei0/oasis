5ee7b3a02f897701d69999b73ecaf123696c6e74
Lei Peng <plhitsz@outlook.com>
2026-03-17 15:16:40 +0800

    Refined quic perf test

--- src/testsuites/test_quic_perf.py
+++ src/testsuites/test_quic_perf.py
@@ -68,62 +68,58 @@ class QuicPerfTest(ITestSuite):
         client = hosts[self.config.client_host]
         server = hosts[self.config.server_host]
 
-        receiver_ip, receiver_port = self.resolve_receiver(network, proto_info)
-
         logging.info(
             "############### Oasis QuicPerfTest from %s to %s ###############",
             client.name(), server.name())
         return self._run_quic_perf(
             client,
             server,
-            (receiver_ip, receiver_port),
             (
                 proto_info.get_protocol_args(network),
                 proto_info.get_protocol_name(),
             ),
         )
 
-    def _run_quic_perf(self, client, server, receiver, protocol):
+    def _run_quic_perf(self, client, server, protocol):
         """Start the quic_perf server, run the client, then clean up."""
         if self.config is None:
             logging.error("QuicPerfTest config is None.")
             return False
-        receiver_ip, receiver_port = receiver
         proto_args, proto_name = protocol
         base_path = os.path.dirname(os.path.abspath(self.result.record))
         # --- start server ---------------------------------------------------
-        for intf in server.getIntfs():
-            server_log_path = os.path.join(
-                base_path, f"{proto_name}/log/{server.name()}_quic_perf.log")
-            logging.info("QuicPerfTest server %s intf %s IP %s", server.name(), intf.name, intf.ip)
-            server_cmd = (
-                f'quic_perf --mode server --addr {intf.ip}'
-                f' --log {server_log_path}'
-                f' --log-interval {self.result.record}'
-            )
-            if receiver_port:
-                server_cmd += f' --port {receiver_port}'
-            if proto_args:
-                server_cmd += f' {proto_args}'
-            if self.config.multipath:
-                server_cmd += ' --multipath'
-            logging.info('quic_perf server cmd: %s', server_cmd)
-            server.cmd(f'{server_cmd} &')
+        server_log_path = os.path.join(
+            base_path, f"{proto_name}/log/{server.name()}_quic_perf.log")
+        server_cmd = (
+            f'quic_perf --mode server --addr 0.0.0.0'
+            f' --log {server_log_path}'
+            f' --log-interval {self.result.record}'
+        )
+        if self.config.multipath:
+            server_cmd += ' --multipath'
+        if proto_args:
+            server_cmd += f' {proto_args}'
+        logging.info('quic_perf server cmd: %s', server_cmd)
+        server.cmd(f'{server_cmd} &')
         time.sleep(1)  # give the server a moment to bind
 
         # --- run client ------------------------------------------------------
         duration = self.config.interval * self.config.interval_num
         client_log_path = os.path.join(
             base_path, f"{proto_name}/log/{client.name()}_quic_perf.log")
-        client_cmd = f'quic_perf --mode client --addr {receiver_ip}'
-        if receiver_port:
-            client_cmd += f' --port {receiver_port}'
+        client_cmd = f'quic_perf --mode client'
+        if self.config.multipath:
+            client_cmd += ' --server-list'
+            for intf in server.getIntfs():
+                logging.info("QuicPerfTest client connect to %s intf %s IP %s", 
+                        server.name(), intf.name, intf.ip)
+                client_cmd += f' {intf.ip}'
+        else:
+            client_cmd += f' --addr {server.IP()}'
         if proto_args:
             client_cmd += f' {proto_args}'
         if self.config.args:
             client_cmd += f' {self.config.args}'
-        if self.config.multipath:
-            client_cmd += ' --multipath'
         client_cmd += f' --count 0 --duration {duration}'
         client_cmd += f' --log {client_log_path}'
         logging.info('quic_perf client cmd: %s', client_cmd)
